e7e6d056a96922812cd233d259d8ab5a934d51e2
XQK <156826106+Jett-X-QK@users.noreply.github.com>
2025-11-27 15:55:34 +0800

    Initial commit

---
+++ include/head.h
@@ -0,0 +1,5 @@
+#ifndef HEAD_H
+#define HEAD_H
+#include <iostream>
+
+#endif // HEAD_H
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "head.h"
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    a = 90;
+    b = 80;
+    int c = a + b;
+    printf("%d\n", c);
+    return 0;
+}
